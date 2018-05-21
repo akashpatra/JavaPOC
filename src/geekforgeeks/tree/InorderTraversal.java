7cd68703f602e181d178d3bceadb9b05da363bd9
Akash Patra <patraakash9@gmail.com>
2018-05-21 20:21:43 +0530

    TREE: Inorder Traversal

---
+++ src/geekforgeeks/tree/InorderTraversal.java
@@ -0,0 +1,30 @@
+package geekforgeeks.tree;
+
+/**
+ * Inorder Traversal of a Tree, using RECURSION
+ *
+ * @author Akash Patra
+ */
+public class InorderTraversal {
+
+    public void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.data + " ");
+            inorderRec(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        InorderTraversal traversal = new InorderTraversal();
+        traversal.inorderRec(root);
+    }
+}
--- src/geekforgeeks/tree/Node.java
+++ src/geekforgeeks/tree/Node.java
@@ -4,8 +4,8 @@ package geekforgeeks.tree;
  * @author Akash Patra
  */
 public class Node {
-    int data;
-    Node left, right;
+    public int data;
+    public Node left, right;
 
     public Node(int item) {
         this.data = item;
