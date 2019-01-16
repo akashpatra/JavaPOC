5f7c792c90b576b3e11ef8ee17d0969d92360b80
Akash Patra <patraakash9@gmail.com>
2019-01-16 18:07:34 +0530

    DS: Linked List BluePrint

---
+++ src/ds/linkedlist/LinkedList.java
@@ -0,0 +1,38 @@
+package ds.linkedlist;
+
+/**
+ * Simple Linked List with Traversal
+ *
+ * @author Akash Patra
+ */
+public class LinkedList {
+    // Head of List
+    public Node head;
+
+    // Linked List Node
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList lList = new LinkedList();
+
+        lList.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+
+        lList.head.next = second;
+        second.next = third;
+
+        Node n = lList.head;
+        while (null != n) {
+            System.out.println("Node Value: " + n.data);
+            n = n.next;
+        }
+    }
+}
