4f1bc4e2e17c4f0c476ffe57ffc9b1e483d1854e
Akash Patra <patraakash9@gmail.com>
2019-05-15 22:42:02 +0530

    Count LeafNodes or Non-Leaf Nodes using Recursion

---
+++ src/geekforgeeks/tree/CountLeafNodesRecursion.java
@@ -0,0 +1,41 @@
+package geekforgeeks.tree;
+
+/**
+ * Total Leaf nodes using Recursion
+ *
+ * @author Akash Patra
+ */
+public class CountLeafNodesRecursion {
+
+    //
+    Node root;
+
+    public CountLeafNodesRecursion() {
+        root = null;
+    }
+
+    public int findTotalLeafNodes(Node root) {
+        if (root == null) {
+            return 0;
+        } else if (root.left == null && root.right == null) {
+            return 1;
+        } else {
+            return findTotalLeafNodes(root.left) + findTotalLeafNodes(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        root.right.left.left = new Node(8);
+        root.right.left.right = new Node(9);
+
+        CountLeafNodesRecursion sumOfAllNodesRecursion = new CountLeafNodesRecursion();
+        System.out.println(sumOfAllNodesRecursion.findTotalLeafNodes(root));
+    }
+}
---
+++ src/geekforgeeks/tree/CountNonLeafNodesRecursion.java
@@ -0,0 +1,42 @@
+package geekforgeeks.tree;
+
+/**
+ * Total Non-Leaf nodes using Recursion
+ *
+ * @author Akash Patra
+ */
+public class CountNonLeafNodesRecursion {
+
+    //
+    Node root;
+
+    public CountNonLeafNodesRecursion() {
+        root = null;
+    }
+
+    public int findTotalNonLeafNodes(Node root) {
+        if (root == null) {
+            return 0;
+        } else if (root.left == null && root.right == null) {
+            return 0;
+        } else {
+            return 1 + findTotalNonLeafNodes(root.left) + findTotalNonLeafNodes(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        root.right.left.left = new Node(8);
+        root.right.left.right = new Node(9);
+        root.right.right.left = new Node(10);
+
+        CountNonLeafNodesRecursion sumOfAllNodesRecursion = new CountNonLeafNodesRecursion();
+        System.out.println(sumOfAllNodesRecursion.findTotalNonLeafNodes(root));
+    }
+}
