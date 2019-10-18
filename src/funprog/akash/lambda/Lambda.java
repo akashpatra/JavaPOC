13d8887c3d99f2c242e1d040fedc0184e620287e
Akash Patra <patraakash9@gmail.com>
2019-10-18 18:54:05 +0530

    JAVA: Playing with Lamda

--- src/funprog/akash/lambda/Lambda.java
+++ src/funprog/akash/lambda/Lambda.java
@@ -4,13 +4,16 @@ package funprog.akash.lambda;
  * @author Akash Patra
  */
 public class Lambda {
+    public int x = 0;
+
     public static void main(String[] args) {
-        sayHello();
+        staticSayHello();
+        new Lambda().sayHello();
 
         executeAdd();
     }
 
-    private static void sayHello() {
+    private static void staticSayHello() {
         IShow iShow = name -> "Hello, " + name;
         System.out.println(iShow.show("akash"));
     }
@@ -19,5 +22,16 @@ public class Lambda {
         IAdd iAdd = (int a, int b) -> (a + b);
         System.out.println(iAdd.add(2, 4));
 //        String.valueOf()
+    }
+
+    private void sayHello() {
+        String name;
+        // Scoping
+        IShow iShow = new IShow() {
+            @Override
+            public String show(String name) {
+                return "Hello, " + name + x;
+            }
+        };
     }
 }
